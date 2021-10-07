0737a8d3bbb4ef9f87bc56d8f44f4f66dbed0290
Miltarius <90238349+Miltarius@users.noreply.github.com>
2021-10-07 20:34:06 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,12 +1,12 @@
-X=int(input('Введите число X'))
-Y=int(input('Введите число Y'))
-if X>0<Y:
+X = int(input('Введите число X'))
+Y = int(input('Введите число Y'))
+if X > 0 < Y:
     print('первая четверть')
-elif X<0<Y:
+elif X < 0 < Y:
     print('вторая четверть')
-elif X<0>Y:
+elif X < 0 > Y:
     print('третья четверть') 
-elif X>0>Y  :     
+elif X > 0 > Y  :     
     print('четвертая четверть')
 else :
     print('точка на оси')    
