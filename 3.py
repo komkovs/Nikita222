2f684cf51bde5a1d7a506dcff8362abb17167ece
komkovs <90065990+komkovs@users.noreply.github.com>
2021-10-09 10:02:21 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -2,5 +2,5 @@ from math import sqrt
 x = int(input('Введите число x'))
 y = int(input('Введите число y'))
 z = int(input('Введите число z'))
-w = sqrt(x * x + y * y + z * z)
+w = sqrt(x**2 + y**2 + z**2)
 print(w)
